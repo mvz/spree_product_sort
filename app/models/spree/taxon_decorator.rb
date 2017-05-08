37a8ec4e080c8ed5665cb131eea3235f1e8eaa76
Jacob Spizziri <jacob.spizziri@gmail.com>
2017-05-08 17:55:29 -0500

    feat(positions): Standardize name diaplay

--- app/models/spree/taxon_decorator.rb
+++ app/models/spree/taxon_decorator.rb
@@ -3,6 +3,10 @@ Spree::Taxon.class_eval do
   has_many :products, -> { order('spree_product_taxons.position') }, through: :product_taxons
 
   def name_and_permalink
-    "#{self.parent.name} -> #{self.name}"
+    if self.parent
+      "#{self.parent.name} -> #{self.name}"
+    else
+      "#{self.name}"
+    end
   end
 end
