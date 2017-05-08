5d7627f04dc70ccb766e1c5da82e37bac47fe041
Jacob Spizziri <jacob.spizziri@gmail.com>
2017-05-08 17:56:37 -0500

    style(position): update method name to be more logical

--- app/models/spree/taxon_decorator.rb
+++ app/models/spree/taxon_decorator.rb
@@ -2,7 +2,7 @@ Spree::Taxon.class_eval do
   has_many :product_taxons, -> { order('spree_product_taxons.position') }
   has_many :products, -> { order('spree_product_taxons.position') }, through: :product_taxons
 
-  def name_and_permalink
+  def taxon_hierarchy_display
     if self.parent
       "#{self.parent.name} -> #{self.name}"
     else
